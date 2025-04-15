5ad5c2f779c1b6b0c51042c256ba7946580edffd
paulpaul168 <github@paulhoeller.at>
2025-04-15 09:31:21 +0000

    Optimize member retrieval in PodioAPI by implementing pagination and filtering for null values in fields.

--- podio_api.py
+++ podio_api.py
@@ -18,15 +18,18 @@ class PodioAPI:
         )
 
     def get_new_members(self) -> list:
-
-        spaceteam_members = self.c.Application.get_items(24519593)["items"]
+        count = self.c.Application.get_items("24519593/count")["count"]
         members_which_need_to_be_created = []
-
-        for member in spaceteam_members:
-            if "null" in member["fields"][0]["values"][0]["value"]:
-                members_which_need_to_be_created.append(member)
+        for i in range(0, int(count / 30) + 1):
+            result = self.c.Item.filter(
+                24519593, attributes={"limit": 30, "offset": i * 30}
+            )
+            if "items" in result: 
+                for item in result["items"]:
+                    if "null" in item["fields"][0]["values"][0]["value"]:
+                        members_which_need_to_be_created.append(item)
         return members_which_need_to_be_created
-
+    
     def get_all_members(self) -> list:
         count = self.c.Application.get_items("24519593/count")["count"]
         results = []
